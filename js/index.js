a86d71a69e9e8919e52e3a9d63dfb8a33a73ec7d
jacqueline ngabo <ngabo.jay@gmail.com>
2022-09-24 08:14:10 +0300

    add changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 const game = ()=>{
     let pSore = 0;
     let cScore = 0;
-}
+    // start game
+    const startGame = () =>{
+        const playbtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match")
+        playbtn.addEventListener("click", (j) => {
+            j.preventDefault
+            introScreen.classList.add(".fadeOut");
+            match.classList.add("fadein");
+
+
+        });
+
+    };
+    const playMatch = () =>{
+        const options = document.querySelectorAll('. options button')
+
+    }
+    startGame();
+};
+game();
